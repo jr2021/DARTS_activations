8513ff111a09d3ebbc5cf4a9e72304fe3d5bf317
lukas <lukas.j.m.strack@gmail.com>
2022-07-07 10:56:24 +0200

    add ResNet20Simple search space

--- RNNResNet20.py
+++ RNNResNet20.py
@@ -311,7 +311,7 @@ class ActivationFuncResNet20SearchSpace(Graph):
 
 config = utils.get_config_from_args(config_type='nas')
 config.optimizer = 'darts'
-config.search.batch_size = 16
+config.search.batch_size = 32
 utils.set_seed(config.seed)
 clear_output(wait=True)
 utils.log_args(config)
