8de6cc37a10aa0c72ed7053032ae9f7c1f59aa1b
lukas <lukas.j.m.strack@gmail.com>
2022-08-01 19:44:20 +0200

    ResNet for evaluation

--- plot_novel_activation.py
+++ plot_novel_activation.py
@@ -1,30 +1,15 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-from activation_sub_func.experimental_func import DartsFunc_complex, DartsFunc_simple
+from activation_sub_func.experimental_func_r import DrNasFunc_complex_r, DrNasFunc_simple_r
 import torch
 import torch.nn as nn
 
 from activation_sub_func.binary_func import SigMul, BetaMix, Mul, Stack, Add
 from activation_sub_func.unary_func import Cosh, Exp, Abs_op
 
-lin = torch.linspace(-15, 15, 1000).reshape((1, 1, 1, 1000))
-activation_func = DartsFunc_complex(1)
-activation_func.binary_2.beta = nn.Parameter(activation_func.binary_2.beta - 0.7)
-
-res = activation_func(lin).detach().numpy().flatten()
-lin = lin.detach().numpy().flatten()
-
-print(lin)
-print(res)
-
-plt.plot(lin, res)
-plt.show()
-
-
 lin = torch.linspace(-5, 5, 1000).reshape((1, 1, 1, 1000)).cuda()
-activation_func = DartsFunc_simple(1).cuda()
-activation_func.binary_1.beta = nn.Parameter(activation_func.binary_1.beta - 0.7)
+activation_func = DrNasFunc_complex_r(1).cuda()
 
 res = activation_func(lin).cpu().detach().numpy().flatten()
 lin = lin.cpu().detach().numpy().flatten()
@@ -33,4 +18,23 @@ print(lin)
 print(res)
 
 plt.plot(lin, res)
+plt.title("DrNas Complex\n"
+          "min(-x, sqrt(x)) + x + beta")
+plt.savefig(f"figures/big.png", dpi=300)
+plt.show()
+
+
+lin = torch.linspace(-5, 5, 1000).reshape((1, 1, 1, 1000)).cuda()
+activation_func = DrNasFunc_simple_r(1).cuda()
+
+res = activation_func(lin).cpu().detach().numpy().flatten()
+lin = lin.cpu().detach().numpy().flatten()
+plt.title("DrNas Simple\n"
+          "max(0, x) + max(0, x)")
+
+print(lin)
+print(res)
+
+plt.plot(lin, res)
+plt.savefig("figures/samll.png", dpi=300)
 plt.show()
